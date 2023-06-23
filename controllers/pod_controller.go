0f3b1d6f23f91f6b9a2b71d7468ee294477f874c
Avery Blanchard <avery.blanchard@ibm.com>
2023-06-23 18:52:56 +0000

    Outline controller logic
    Signed-off-by: Avery Blanchard <avery.blanchard@ibm.com>

--- controllers/pod_controller.go
+++ controllers/pod_controller.go
@@ -38,17 +38,24 @@ type PodReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Pod object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
 func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	var pod corev1.Pod
+   	if err := r.Get(ctx, req.NamespacedName, &pod); err != nil {
+		if apierrors.IsNotFound(err) {
+            		return ctrl.Result{}, nil
+        	}
+        	log.Error(err, "unable to fetch Pod")
+       	 	return ctrl.Result{}, err
+    	}
+	// iterate over pod containers
+	// parse container ID
+	// use crictl or docker top to grab PID
+	// parse NS from PID
+	// add container annotations for both 
 
 	return ctrl.Result{}, nil
 }
